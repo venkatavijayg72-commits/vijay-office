4058a38d3ba64e330871cfd6de83e44ca5794667
venkatavijayg72-commits <venkatavijayg72@gmail.com>
2025-11-07 09:47:29 +0530

    team task

---
+++ team task 1/script.js
@@ -0,0 +1,32 @@
+let city = "banglore";
+let age = 25;
+let country = "India";
+
+let1 = "vijay"
+let2 = "surya"
+
+console.log(city);
+console.log(age);
+console.log(country);
+
+// Just reassign values, don't redeclare
+city = "chennai";
+age = "29";
+country = "Usa";
+
+console.log(let1 + let2);
+
+// Change these to reassignments instead of redeclarations
+city = "hyderabad";
+age = "33";
+country = "India";
+
+city = "chennai"
+age = "40"
+country = "Usa"
+
+country = "russia"
+
+let village = "vijayawada"
+
+console.log(village);
